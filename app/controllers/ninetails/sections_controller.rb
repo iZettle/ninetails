90ab8968afaa1b1b75d9b00dfb6704d5b76d6288
Martin Liberg <martin.liberg@izettle.com>
2015-12-02 13:08:29 +0100

    Let /sections return entire sections
    Not just the type, but also their elements and tags.

--- app/controllers/ninetails/sections_controller.rb
+++ app/controllers/ninetails/sections_controller.rb
@@ -3,14 +3,20 @@ module Ninetails
 
     def index
       sections = Rails.root.join("app", "components", "section").children.collect do |entry|
-        entry.basename.to_s.sub(/(\..*)$/, '').classify
+        empty_section_from_name entry.basename.to_s.sub(/(\..*)$/, '')
       end
 
       render json: { sections: sections }
     end
 
     def show
-      render json: "Section::#{params[:id]}".safe_constantize.new.serialize
+      render json: empty_section_from_name(params[:id])
+    end
+
+    private
+
+    def empty_section_from_name(id)
+      "Section::#{id.classify}".safe_constantize.new.serialize
     end
 
   end
--- spec/requests/sections_spec.rb
+++ spec/requests/sections_spec.rb
@@ -5,7 +5,8 @@ describe "Sections API" do
   it "should list the available sections" do
     get "/sections"
     expect(response).to be_success
-    expect(json["sections"]).to include "Billboard"
+    billboardJson = json["sections"].find { |s| s["type"] == "Billboard" }.to_json
+    expect(billboardJson).to eq Section::Billboard.new.serialize.to_json
   end
 
   it "should be possible to get a section's structure" do
