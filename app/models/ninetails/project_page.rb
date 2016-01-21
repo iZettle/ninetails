8335f0f9d119487046dd6713c86b78981e758b6a
Damien <damien@izettle.com>
2016-01-21 16:45:44 +0100

    Validate project pages have a project and a page

--- app/models/ninetails/project_page.rb
+++ app/models/ninetails/project_page.rb
@@ -5,8 +5,24 @@ module Ninetails
     belongs_to :page_revision
     belongs_to :project
 
-    validates :page_revision, uniqueness: { scope: :project, message: "has already been set for this page in the project" }
-    validates :page, uniqueness: { scope: :project, message: "is already used in this project" }
+    validates :project, {
+      presence: true
+    }
+
+    validates :page_revision, {
+      uniqueness: {
+        scope: :project,
+        message: "has already been set for this page in the project"
+      }
+    }
+
+    validates :page, {
+      presence: true,
+      uniqueness: {
+        scope: :project,
+        message: "is already used in this project"
+      }
+    }
 
   end
 end
