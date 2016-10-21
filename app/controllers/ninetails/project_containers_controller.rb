8d6025a10b30736a624ba1bdfd69d4d5ad8e1fbb
Damien <damien@izettle.com>
2016-10-21 13:40:48 +0200

    Eager load project association

--- app/controllers/ninetails/project_containers_controller.rb
+++ app/controllers/ninetails/project_containers_controller.rb
@@ -2,7 +2,7 @@ module Ninetails
   class ProjectContainersController < NinetailsController
 
     def projects
-      @project_containers = ProjectContainer.where(container_id: params[:container_id])
+      @project_containers = ProjectContainer.includes(:project).where container_id: params[:container_id]
     end
 
   end
