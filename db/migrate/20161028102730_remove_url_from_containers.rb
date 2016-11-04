95733d4e2629dc6361ecab243cc4f1d3817dd5e8
Damien <damien@izettle.com>
2016-11-04 13:44:11 +0100

    Update revisions before dropping urls from containers

--- db/migrate/20161028102730_remove_url_from_containers.rb
+++ db/migrate/20161028102730_remove_url_from_containers.rb
@@ -1,5 +1,15 @@
 class RemoveUrlFromContainers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :ninetails_containers, :url, :string
+  def up
+    Ninetails::Page.with_deleted.all.each do |page|
+      page.revisions.each do |revision|
+        revision.update_attributes url: page.url unless revision.url.present?
+      end
+    end
+
+    remove_column :ninetails_containers, :url
+  end
+
+  def down
+    add_column :ninetails_containers, :url, :string
   end
 end
