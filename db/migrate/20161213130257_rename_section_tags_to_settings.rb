18639558b610a212423c77f18bcdc102d53dc0bf
Joakim Jarsäter <joakim@jarsater.com>
2016-12-13 14:33:48 +0100

    add remove column instead of rename

--- db/migrate/20161213130257_rename_section_tags_to_settings.rb
+++ db/migrate/20161213130257_rename_section_tags_to_settings.rb
@@ -1,5 +1,6 @@
 class RenameSectionTagsToSettings < ActiveRecord::Migration[5.0]
   def change
-    rename_column :ninetails_content_sections, :tags, :settings
+    remove_column :ninetails_content_sections, :tags, :json
+    add_column :ninetails_content_sections, :settings, :json, default: {}
   end
 end
